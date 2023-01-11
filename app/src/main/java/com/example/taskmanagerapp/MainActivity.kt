2e88414382105c3007d0abd670ffb3dadbe63048
JustStone <kripto.doctor@gmail.com>
2023-01-11 22:52:46 +1100

    Experiment with callbacks between activitys

--- app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
@@ -6,18 +6,29 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.taskmanagerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var MActBinding : ActivityMainBinding
+    private var launcher : ActivityResultLauncher<Intent>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MActBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(MActBinding.root)
+
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result: ActivityResult ->
+            if (result.resultCode == RESULT_OK)
+                MActBinding.HMain.text = result.data?.getStringExtra("callback")
+        }
     }
     fun onClickToTask(viev : View){
-        val intent = Intent(this, TaskActivity::class.java)
+        launcher?.launch(Intent(this, TaskActivity::class.java))
+        intent.putExtra("header_task", MActBinding.btnToTask.text)
         startActivity(intent)
     }
 
--- app/src/main/java/com/example/taskmanagerapp/TaskActivity.kt
+++ app/src/main/java/com/example/taskmanagerapp/TaskActivity.kt
@@ -4,15 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.taskmanagerapp.databinding.ActivityMainBinding
+import com.example.taskmanagerapp.databinding.ActivityTaskBinding
 
 class TaskActivity : AppCompatActivity() {
+    lateinit var TaskActBinding : ActivityTaskBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_task)
+        TaskActBinding = ActivityTaskBinding.inflate(layoutInflater)
+        setContentView(TaskActBinding.root)
+
+        val header_task = intent.getStringExtra("header_task")
+        TaskActBinding.HTask.text = header_task
     }
 
     fun onClickToMain(viev : View){
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+//        val intent = Intent(this, MainActivity::class.java)
+//        startActivity(intent)
+
+        val i = Intent()
+        i.putExtra("callback", "Sucess!")
+        setResult(RESULT_OK, i)
+        finish()
     }
 }
