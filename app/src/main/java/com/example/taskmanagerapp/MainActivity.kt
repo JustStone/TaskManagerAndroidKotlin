ffe8881f79fe111f014dfda28397ae4ff6778dbf
JustStone <kripto.doctor@gmail.com>
2023-01-11 14:34:44 +1100

    add ViewBinding

--- app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
@@ -2,14 +2,34 @@ package com.example.taskmanagerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import com.example.taskmanagerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var MActBinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MActBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(MActBinding.root)
+
+        MActBinding.tvTest.text = "Check"
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 
     override fun onResume() {
         super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
     }
 }
